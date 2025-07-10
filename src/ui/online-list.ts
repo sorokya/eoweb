631a847c15dcf1ddfc6e179471c65a119f283c9e
Dan <coderdan@protonmail.com>
2025-07-10 19:44:52 +0100

    small styling changes

--- src/ui/online-list.ts
+++ src/ui/online-list.ts
@@ -13,18 +13,18 @@ export class OnlineList extends Base {
 
     const scrollHandle = this.container.querySelector('.scroll-handle');
 
-    const topOffset = 37;
-    const bottomOffset = 30;
+    const topOffset = 30;
+    const bottomOffset = 35;
+    const scrollHandleHeight = 15;
 
     playersContainer.addEventListener('scroll', (_) => {
+      const containerHeight = playersContainer.clientHeight;
+      const maxScrollTop = containerHeight - scrollHandleHeight - topOffset;
       const percentageScrolled =
         playersContainer.scrollTop /
         (playersContainer.scrollHeight - playersContainer.clientHeight);
-      const containerHeight = playersContainer.clientHeight;
-      const scrollHandleHeight = 20; // Adjust this value as needed for the height of the scroll handle
-      const maxScrollTop = containerHeight - scrollHandleHeight - bottomOffset;
       const percentageScrolledTop =
-        percentageScrolled * maxScrollTop + topOffset;
+        percentageScrolled * maxScrollTop + bottomOffset;
       (scrollHandle as HTMLElement).style.top = `${percentageScrolledTop}px`;
     });
 
@@ -35,35 +35,42 @@ export class OnlineList extends Base {
       playerCountElement.textContent = `${players.length}`;
       playersContainer.innerHTML = '';
 
-      players.map((player) => {
-        const playerElement = document.createElement('div');
-        playerElement.className = 'player';
+      players
+        .flatMap((p) => Array.from({ length: 10 }).map((_) => p))
+        .map((player) => {
+          const playerElement = document.createElement('div');
+          playerElement.className = 'player';
 
-        const nameElement = document.createElement('span');
-        nameElement.className = 'name';
-        const playerIconElement = document.createElement('div');
-        playerIconElement.className = 'icon';
-        playerIconElement.setAttribute('data-id', (player.icon + 9).toString());
-        const titleElement = document.createElement('span');
-        titleElement.className = 'title';
-        titleElement.textContent = player.title || '-';
-        const guildElement = document.createElement('span');
-        guildElement.className = 'guild';
-        guildElement.textContent =
-          !player.guildTag || player.guildTag === '   ' ? '-' : player.guildTag;
-        const classElement = document.createElement('span');
-        classElement.className = 'class';
-        classElement.textContent =
-          this.client.ecf.classes[player.classId - 1]?.name || '-';
+          const nameElement = document.createElement('span');
+          nameElement.className = 'name';
+          const playerIconElement = document.createElement('div');
+          playerIconElement.className = 'icon';
+          playerIconElement.setAttribute(
+            'data-id',
+            (player.icon + 9).toString(),
+          );
+          const titleElement = document.createElement('span');
+          titleElement.className = 'title';
+          titleElement.textContent = player.title || '-';
+          const guildElement = document.createElement('span');
+          guildElement.className = 'guild';
+          guildElement.textContent =
+            !player.guildTag || player.guildTag === '   '
+              ? '-'
+              : player.guildTag;
+          const classElement = document.createElement('span');
+          classElement.className = 'class';
+          classElement.textContent =
+            this.client.ecf.classes[player.classId - 1]?.name || '-';
 
-        nameElement.textContent = player.name;
-        playerElement.appendChild(playerIconElement);
-        playerElement.appendChild(nameElement);
-        playerElement.appendChild(titleElement);
-        playerElement.appendChild(guildElement);
-        playerElement.appendChild(classElement);
-        playersContainer.appendChild(playerElement);
-      });
+          nameElement.textContent = player.name;
+          playerElement.appendChild(playerIconElement);
+          playerElement.appendChild(nameElement);
+          playerElement.appendChild(titleElement);
+          playerElement.appendChild(guildElement);
+          playerElement.appendChild(classElement);
+          playersContainer.appendChild(playerElement);
+        });
     });
   }
 
