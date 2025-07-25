40dc48e5a27930122c8a8d4fcc9aa994704c7020
sjbmcg <zmmh2430@leeds.ac.uk>
2025-07-25 21:45:34 +0100

    weapon sprite type

---
+++ src/utils/get-weapon-sprite-type.ts
@@ -0,0 +1,12 @@
+export enum WeaponSpriteType {
+  Standing = 1,
+  WalkFrame1 = 3,
+  WalkFrame2 = 4,
+  WalkFrame3 = 5,
+  WalkFrame4 = 6,
+  SpellCast = 11,
+  SwingFrame1 = 13,
+  SwingFrame2 = 14,
+  SwingFrame2Spec = 17,
+  Shooting = 18,
+}
