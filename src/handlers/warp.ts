46d116e7d02fa863e03251a465324018aef723c0
Andrew Choate <a_choate@live.com>
2025-11-15 15:57:55 -0500

    Refactor ui css into component hiearchy

--- src/handlers/warp.ts
+++ src/handlers/warp.ts
@@ -12,7 +12,7 @@ import { getEmf } from '../db';
 import { EOResourceID } from '../edf';
 import { EffectAnimation, EffectTargetCharacter } from '../render/effect';
 import { playSfxById, SfxId } from '../sfx';
-import { ChatIcon } from '../ui/chat';
+import { ChatIcon } from '../ui/chat/chat';
 
 function handleWarpRequest(client: Client, reader: EoReader) {
   const packet = WarpRequestServerPacket.deserialize(reader);
---
+++ src/ui/exit-game/exit-game.ts
@@ -0,0 +1,30 @@
+import mitt from 'mitt';
+import { Base } from '../base-ui';
+
+import './exit-game.css';
+
+type Events = {
+  click: undefined;
+};
+
+export class ExitGame extends Base {
+  protected container = document.getElementById('exit-game');
+  private button: HTMLButtonElement = this.container.querySelector(
+    'button[data-id="exit-game"]',
+  );
+  private emitter = mitt<Events>();
+
+  constructor() {
+    super();
+    this.button.addEventListener('click', () => {
+      this.emitter.emit('click', undefined);
+    });
+  }
+
+  on<Event extends keyof Events>(
+    event: Event,
+    handler: (data: Events[Event]) => void,
+  ) {
+    this.emitter.on(event, handler);
+  }
+}
